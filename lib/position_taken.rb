b9965a243eb6c2c2f9d95cf781c0acc44933c4ff
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-08-11 18:44:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken?(array, index)
   if array[index] == " " || array[index] == "" || array[index] == nil
     return false
+  else if array[index] == "X" || array[index] == "O"
+    return true
   end
 end
